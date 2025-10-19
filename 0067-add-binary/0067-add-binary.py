aa917e44c527cf633f52940244f853e3d699573e
r0sas <135124483+r0sas@users.noreply.github.com>
2025-10-19 17:43:17 +0100

    Time: 5 ms (8.38%), Space: 18.1 MB (13.02%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,21 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        s = 0
+        new = ""
+        if len(a) > len(b):
+            highest = a
+            lowest = b
+        else:
+            highest = b
+            lowest = a
+        for i in range(1, len(lowest)+1):
+            print(i)
+            new = str( (int(highest[-i]) + int(lowest[-i]) + s) %2) + new
+            s = int( (int(highest[-i]) + int(lowest[-i]) + s) /2)
+        for i in range(len(lowest)+1, len(highest)+1):
+            print(i)
+            new = str( (int(highest[-i]) + s) %2) + new
+            s = int( (int(highest[-i]) + s) /2)
+        if s == 1:
+            new = "1" + new
+        return new
